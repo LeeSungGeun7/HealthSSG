da5715a18209ed12b5ae2da4a689accfacdf8560
sk <sungkeno3o@gmail.com>
2024-04-15 17:35:10 +0900

    dd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ import { getToken } from '../api/kakao/route';
 
 
 
-function page() {
+function Page() {
     //const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
     const {data : session} = useSession();
     const route = Router
@@ -43,4 +43,4 @@ function page() {
   )
 }
 
-export default page
+export default Page
